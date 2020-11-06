9a2dc67b89b7e7eb6143aea32086282a67aa5f23
ryanjosepro <ryanjose.pro@gmail.com>
2020-11-06 00:51:53 -0300

    React State

--- src/App.js
+++ src/App.js
@@ -1,9 +1,40 @@
 import React from 'react';
 
+import { createSimpleStore } from 'react-simple-reducer';
+
+const MyStatus = createSimpleStore(
+    {
+        count: 0
+    },
+    {
+        increment(state) {
+            state.count++;
+        },
+
+        descrement(state) {
+            state.count--;
+        }
+    }
+)
+
 function App() {
-  return (
-    <h1>Hello World!</h1>
-  );
+    return (
+        <MyStatus.Provider>
+            <MyStatus.GetState>
+                {
+                state => {
+                    return(
+                    <>
+                    <h1>Total: {state.count}</h1>
+                    <button>-</button>
+                    <button>+</button>
+                    </>
+                    )
+                }
+                }
+            </MyStatus.GetState>
+        </MyStatus.Provider>
+    );
 }
 
 export default App;
